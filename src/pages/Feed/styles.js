da82dc470a3241279d35dc7b1a747e04be97cf91
marcosbarker <marcos_barker@hotmail.com>
2021-07-01 17:37:11 -0300

    style: export

---
+++ src/pages/Feed/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Post = styled.View`
+  
+`;
+
+export const Header = styled.View``;
+
+export const Avatar = styled.View``;
+
+export const Name = styled.Text``;
+
+export const PostImage = styled.Image``;
+
+export const Description = styled.Text``;
